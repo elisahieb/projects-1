7f15aeba6f47a863b8489348ae443130a523d3b8
javauser <javauser@hbc3019s18033.acc.austincc.edu>
2019-10-29 21:39:25 -0500

    Started the sql database stuff

---
+++ Hubbub/web/WEB-INF/bootstrap-derby.sql
@@ -0,0 +1,17 @@
+DROP TABLE users;
+
+CREATE TABLE users (
+    username VARCHAR(20) NOT NULL,
+    passhash CHAR(55) NOT NULL,
+    joined DATE DEFAULT CURRENT_DATE,
+    CONSTRAINT pk_users PRIMARY KEY (username)
+);
+
+CREATE TABLE posts (
+    content VARCHAR(140) NOT NULL,
+    author VARCHAR(20) NOT NULL,
+    posted TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    id INTEGER NOT NULL GENERATED ALWAYS AS IDENTITY,
+    CONSTRAINT pk_posts PRIMARY KEY (id)
+    CONSTRAINT fk_post_author FOREIGN KEY author REFERENCES users(username)
+)
